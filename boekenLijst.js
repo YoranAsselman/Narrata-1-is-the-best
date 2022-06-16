090ae12afee2ad6bc34bd4408311ec158c19529b
Aachal Shrestha <aachal.shrestha@student.ehb.be>
2022-06-16 11:39:03 +0200

    filter+SORTING code klaar, maar nog niet gerendered

--- boekenLijst.js
+++ boekenLijst.js
@@ -1,18 +1,105 @@
 "use strict";
 
+
 import bib from "./boeken.js";
 
 console.log(bib);
+let likedBooks = [];
 
-bib.forEach((boek) => {
-    let shortBeschrijving = boek.beschrijving.substr(0, 100);
-    shortBeschrijving += "...";
-    console.log(shortBeschrijving);
-    document.getElementById('boekenLijst__boekenWrapper').innerHTML += `<div class="boekenLijst__boekenWrapper__boek">
+const bibliotheek = {
+    allBooks: bib,
+    toRenderBooks: [],
+
+    heartEmptyImgPath: "./heart_noFill.png",
+    likedBooks: [],
+    heartButtons: [],
+    sortedBooks: [],
+    userInput: "all",
+
+    sort: false,
+    filter: false,
+    init() {
+        this.render();
+
+        this.addToLiked();
+
+        document.getElementById('boekenLijst__filterAndSort__alfabetischSortButton').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.sortAlphabeticatlly();
+        });
+
+
+
+
+
+        let filterGenre = document.getElementsByName('genre');
+        filterGenre.forEach((book) => {
+            book.addEventListener('click', (event) => {
+                this.userInput = event.target.value;
+                console.log(this.userInput);
+                this.filterByGenre();
+            });
+        });
+    },
+
+    addToLiked() {
+        const heartButtons = document.getElementsByClassName('heartIMG');
+        console.log(heartButtons);
+
+        heartButtons[0].addEventListener('click', () => {
+            console.log('adding to liked');
+            this.heartEmptyImgPath = "./filled_heart.png";
+            console.log(this.heartEmptyImgPath);
+            this.render();
+        });
+
+        console.log(this.sort);
+
+    },
+
+    filterByGenre() {
+        console.log('filtering');
+        const filteredBooks = this.allBooks.filter((e) => {
+            if (this.userInput == "all") {
+                return this.allBooks;
+            }
+            if (e.genre == this.userInput) {
+                return true;
+            }
+
+        });
+        this.filteredBooks = filteredBooks;
+        console.log(filteredBooks);
+
+        this.filter = true;
+        this.render();
+
+
+    },
+
+
+    render() {
+        this.toRenderBooks = this.allBooks;
+
+        if (this.sort) {
+            this.toRenderBooks = this.sortedBooks;
+            console.log('sorting');
+        }
+        if (this.filter) {
+            this.toRenderBooks = this.filteredBooks;
+            console.log('filtering');
+        }
+
+        this.toRenderBooks.forEach((boek) => {
+            console.log('rendering');
+            let shortBeschrijving = boek.beschrijving.substr(0, 100);
+            shortBeschrijving += "...";
+
+            document.getElementById('boekenLijst__boekenWrapper').innerHTML += `<div class="boekenLijst__boekenWrapper__boek">
     <div>
         <img src="${boek.image_url}"  class ="boekenLijst__boekenWrapper__boek__img">
         <div class="boekenLijst__boekenWrapper__boek__img__heart">
-            <img src ="./heart_noFill.png">
+            <img src =${this.heartEmptyImgPath} class ="heartIMG">
         </div>
     </div>
     <div class="boekenLijst__boekenWrapper__boek__info">
@@ -28,4 +115,39 @@ bib.forEach((boek) => {
         </div>
     </div>
     </div>`;
-});
+        });
+
+        this.filter = false;
+        this.sort = false;
+    },
+
+    showLiked() {
+
+    },
+
+    sortAlphabeticatlly() {
+
+        const soortedBooks = bib.sort((a, b) => {
+            if (a.titel < b.titel) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        this.sortedBooks = soortedBooks;
+        this.sort = true;
+        console.log(this.sort);
+
+        this.render();
+
+    }
+
+
+};
+
+bibliotheek.init();
+
+//display liked list
+
+
+//onclick add to liked list list
